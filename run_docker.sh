3c9623ceb32de4fa180e9f87a142f77f6ed937cc
Alex Wetzler <alex.wetzler@outlook.com>
2020-06-22 00:18:54 +0000

    testing of pipeline build with Jenkinsfile #2

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker build -t my-nginx-app .
 docker image ls
-docker run --rm -it -p 8081:80 my-nginx-app
+#docker run --rm -it -p 8081:80 my-nginx-app
 
 #docker rmi <image>:<tag>
 #curl localhost:8081
